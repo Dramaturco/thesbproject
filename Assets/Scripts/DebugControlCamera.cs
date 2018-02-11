86b6f4aed02a00341c4a9b4c01b5f64e30e480e7
Emre Izgoel <izgoel@e-spirit.com>
2018-02-11 10:26:23 +0100

    initial commit added background, camera and some basic camera movement using arrow keys for debugging purposes the keys will be changed from GetAxis to Input.GetKeyDown, so the player can be moved while in debug mode

---
+++ Assets/Scripts/DebugControlCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugControlCamera : MonoBehaviour {
+	public float cameraSpeed = 0.5f;
+	public bool debugMode = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (debugMode) {
+			this.transform.position = moveCamera (this.transform.position);
+		}
+	}
+	Vector3 moveCamera(Vector3 initPos){
+		Vector3 newPos = initPos;
+		newPos.x += Input.GetAxis ("Horizontal") * cameraSpeed;
+		newPos.y += Input.GetAxis ("Vertical") * cameraSpeed;
+		return newPos;
+	}
+}
