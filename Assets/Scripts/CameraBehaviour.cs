e414f2470dfbc51cfb49492ee1a5047f84d7ea4b
Emre Izgoel <izgoel@e-spirit.com>
2018-02-11 12:13:27 +0100

    added player and landingzone, player can move in any direction and falls using rigidbody2D physics

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+	GameObject[] players;
+	// Use this for initialization
+	void Start () {
+		players = GameObject.FindGameObjectsWithTag ("Player");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float minY = 500f;
+		foreach (GameObject player in players) {
+			float playerY = player.transform.position.y;
+			if (playerY < minY) {
+				minY = playerY;
+			}
+		}
+		updateCameraY (minY);
+	}
+	void updateCameraY(float newY){
+		Vector3 cameraPos = this.transform.position;
+		cameraPos.y = newY;
+		this.transform.position = cameraPos;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	public float playerSpeed = 0.5f;
+	public float maxSpeed = 20.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = movePlayer (this.transform.position);
+		Rigidbody2D playerBody = this.GetComponentInChildren<Rigidbody2D> ();
+		limitSpeed (playerBody);
+	}
+	Vector3 movePlayer(Vector3 initPos){
+		Vector3 newPos = initPos;
+		newPos.x += Input.GetAxis ("Horizontal") * playerSpeed;
+		newPos.y += Input.GetAxis ("Vertical") * playerSpeed;
+		return newPos;
+	}
+	void limitSpeed(Rigidbody2D body){
+		Debug.Log (this + " speed: " + body.velocity.magnitude);
+		if (body) {
+			if (body.velocity.magnitude > maxSpeed) {
+				body.velocity = body.velocity.normalized * maxSpeed;
+			}
+		}
+	}	
+}
